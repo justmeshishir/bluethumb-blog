f51650d8630c23a72e539e3e5f9855499aa40ddf
Shishir Thapa <shishirthapa1@gmail.com>
2021-09-17 10:10:52 +0545

    refactor for httpparty

--- app/services/bluethumb_service.rb
+++ app/services/bluethumb_service.rb
@@ -1,45 +1,57 @@
 # frozen_string_literal: true
-require 'net/http'
 
 class BluethumbService
-  BASE_URL = 'http://localhost:4000'.freeze
+  include HTTParty
+  base_uri 'http://localhost:4000'
 
-  class << self
-    def posts
-      uri = URI("#{BASE_URL}/posts")
-      response = Net::HTTP.get(uri)
-      JSON.parse(response)
-    end
-
-    def create_post(payload)
-      uri = URI("#{BASE_URL}/posts")
-      response = Net::HTTP.post_form(uri, post: payload)
-      JSON.parse(response.body)
-    end
-
-    def find_post(post_id)
-      uri = URI("#{BASE_URL}/posts/#{post_id}")
-      response = Net::HTTP.get(uri)
-      post = JSON.parse(response)
-      comments = find_comments(post['comments_path'])
-      post['comments'] = comments
-
-      post
-    end
-
-    def create_comment(post_id, payload)
-      uri = URI("#{BASE_URL}/posts/#{post_id}/comments")
-      response = Net::HTTP.post_form(uri, payload)
-      JSON.parse(response.body)
-    end
-
-    private
-
-    def find_comments(path)
-      uri = URI("#{BASE_URL}#{path}")
-      response = Net::HTTP.get(uri)
-      JSON.parse(response)
-    end
+  def initialize(id: nil, payload: nil)
+    @post_id = id
+    @payload = payload
   end
 
+  def posts
+    response = self.class.get('/posts')
+    return [] unless response['posts'].present?
+
+    response['posts']
+  end
+
+  def create_post
+    payload = {
+      "post": {
+        "title": @payload['title'],
+        "body": @payload['body']
+      }
+    }
+
+    response = self.class.post('/posts', body: payload)
+    return nil if response.body.blank?
+
+    JSON.parse(response.body)['post']
+  end
+
+  def find_post
+    post = self.class.get("/posts/#{@post_id}")
+    comments = find_comments(post['comments_path'])
+    post.merge!(comments)
+
+    post
+  end
+
+  def create_comment
+    payload = {
+      "comment": {
+        "name": @payload['name'],
+        "body": @payload['body']
+      }
+    }
+
+    self.class.post("/posts/#{@post_id}/comments", body: payload)
+  end
+
+  private
+
+  def find_comments(comments_path)
+    self.class.get(comments_path)
+  end
 end
