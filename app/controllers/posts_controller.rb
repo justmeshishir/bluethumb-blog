82d8f3ea44dd93b252155dc8e5da41999c616c55
Shishir Thapa <shishirthapa1@gmail.com>
2021-09-17 10:12:17 +0545

    posts actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,23 @@
 class PostsController < ApplicationController
   def index
-    @posts = BluethumbService.posts
+    @posts = BluethumbService.new.posts
+  end
+
+  def new; end
+
+  def create
+    post = BluethumbService.new(payload: post_params).create_post
+
+    redirect_to post_path(post['id'])
   end
 
   def show
-    @post = BluethumbService.find_post(params[:id]) if params[:id].present?
+    @post = BluethumbService.new(id: params[:id]).find_post if params[:id].present?
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :body)
   end
 end
